99edf2731ee95943a94eb7643f495020d3c62815
devsingh1234 <52194749+devsingh1234@users.noreply.github.com>
2020-02-21 22:04:55 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the two numbers to be miltiplied");
+scanf("%d%d",&a,&b);
+c=a*b;
+prirntf("the number is %d\n");
+printf("THANKYOU\n");
