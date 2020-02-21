a4408d729da4a7db63f9b1679d5d53bd9c67ca4e
devsingh1234 <52194749+devsingh1234@users.noreply.github.com>
2020-02-21 21:47:16 +0530

    Update and rename README.md to addtwonumbers.c

---
+++ addtwonumbers.c
@@ -0,0 +1,11 @@
+# coders-place
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("hello\n plzz enter the number to be added");
+scanf("%d",&d);
+c=a+b;
+printf("the sum is %d",c);
+{
